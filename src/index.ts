3587dc1998625d809d28d16f0a8744584ad9ee73
chuwi <srpc9682@protonmail.com>
2026-01-21 13:48:51 +0100

    OrderPizzaNode

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ class Pedido {
 
 }
 
-pedido: Array<PizzaInfo> = []
+let pedido: Array<ItemPizza> = []
 
 function ComposePizzaNode(pizza: PizzaInfo, onClick: () => void): HTMLDivElement {
    let root = document.createElement("div");
@@ -48,18 +48,18 @@ function ComposePizzaNode(pizza: PizzaInfo, onClick: () => void): HTMLDivElement
    let column = document.createElement("div")
    column.classList = "column"
 
-   let name = document.createElement("p")
+   let name = document.createElement("h2")
    name.innerText = pizza.name
    column.appendChild(name)
 
    let alergensRow = document.createElement("div")
    alergensRow.classList = "row"
 
-      for (let alergen of pizza.alergens) {
-         let alergenNode = document.createElement("img")
-         alergenNode.src = alergen.toString()
-         alergensRow.appendChild(alergenNode)
-      }
+   for (let alergen of pizza.alergens) {
+      let alergenNode = document.createElement("img")
+      alergenNode.src = alergen.toString()
+      alergensRow.appendChild(alergenNode)
+   }
 
    column.appendChild(alergensRow)
 
@@ -74,6 +74,46 @@ function ComposePizzaNode(pizza: PizzaInfo, onClick: () => void): HTMLDivElement
    return root
 }
 
+function ComposeOrderNode(pizza: PizzaInfo, onClick: () => void): HTMLDivElement {
+
+   let root = document.createElement('div');
+   root.classList = 'inner-card card';
+
+   let pizzaImage = document.createElement("img")
+   pizzaImage.src = pizza.image.toString()
+
+   root.appendChild(pizzaImage)
+
+   let column = document.createElement("div")
+   column.classList = "column"
+
+   let name = document.createElement('h3');
+   name.innerHTML = pizza.name;
+   column.appendChild(name);
+
+   let count = document.createElement('input');
+   count.type = 'number';
+   column.appendChild(count)
+
+   let price = document.createElement('p');
+   price.innerHTML = pizza.price.toString();
+   column.appendChild(price);
+
+   let button = document.createElement('button');
+   button.classList = 'button';
+   button.innerHTML = 'Quitar 1';
+   button.addEventListener('click', () => onClick());
+   column.appendChild(button);
+
+   let totalPrice = document.createElement('p');
+   totalPrice.innerHTML = (count.valueAsNumber * pizza.price).toString();
+   column.appendChild(totalPrice);
+
+   root.appendChild(column);
+
+   return root
+}
+
 function test_newNodoPizza() {
    let elegir_pizza_node = document.getElementById("menu-pizzas")
    elegir_pizza_node?.appendChild(
@@ -84,7 +124,18 @@ function test_newNodoPizza() {
                12.0,
                "Pizza vectorial"
            ),
-           () => { alert("Pizza vectorial") }
+           () => { alert("Pizza vectorial"),
+           ComposeOrderNode(
+               new PizzaInfo(
+                   "https://upload.wikimedia.org/wikipedia/commons/4/45/Pizza_slice.png",
+                   [],
+                   12.0,
+                   "Pizza vectorial"
+               ),
+               () => {
+                  alert('Si')
+               }
+           )}
        )
    )
 }
